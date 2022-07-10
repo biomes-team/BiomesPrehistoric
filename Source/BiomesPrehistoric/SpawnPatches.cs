1a2283a3668990685cce0a933edbc57a2edcb303
Zylleon <56230821+Zylleon@users.noreply.github.com>
2022-07-09 20:48:33 -0600

    patch to apply "dino world" to plants (disabled)

--- Source/BiomesPrehistoric/SpawnPatches.cs
+++ Source/BiomesPrehistoric/SpawnPatches.cs
@@ -29,4 +29,31 @@ namespace BiomesPrehistoric
             return true;
         }
     }
+
+
+
+    // Uncomment this patch for Dino World to apply to plants
+
+    /*
+    [HarmonyPatch(typeof(BiomeDef), "CommonalityOfPlant")]
+    public static class PlantSpawnPatch
+    {
+        static bool Prefix(PawnKindDef plantDef, List<BiomeAnimalRecord> ___wildPlants, ref float __result)
+        {
+            if (BiomesPrehistoricMod.mod.settings.dinoOnly)
+            {
+                if (___wildPlants.Any(d => d.animal.modContentPack.Name == "Biomes! Prehistoric"))
+                {
+                    if (plantDef.modContentPack.Name != "Biomes! Prehistoric")
+                    {
+                        __result = 0f;
+                        return false;
+                    }
+                }
+            }
+
+            return true;
+        }
+    }
+    */
 }
