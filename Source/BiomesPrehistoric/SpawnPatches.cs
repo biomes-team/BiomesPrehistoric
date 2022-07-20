55a7909e40d513e9c5eb26897e857bcd7981d394
Zylleon <56230821+Zylleon@users.noreply.github.com>
2022-07-19 18:25:03 -0600

    Fixes to dino world option
    Traders will only spawn with dino pack animals
    Manhunter packs will only be dinos

--- Source/BiomesPrehistoric/SpawnPatches.cs
+++ Source/BiomesPrehistoric/SpawnPatches.cs
@@ -31,6 +31,106 @@ namespace BiomesPrehistoric
     }
 
 
+    /// <summary>
+    /// prevents manhunter non-dinos when dinoworld is enabled
+    /// </summary>
+    [HarmonyPatch(typeof(ManhunterPackIncidentUtility), "TryFindManhunterAnimalKind")]
+    public static class ManhunterAnimalPatch
+    {
+        static bool Prefix(float points, int tile, ref PawnKindDef animalKind, ref bool __result)
+        {
+
+            if (!BiomesPrehistoricMod.mod.settings.dinoOnly)
+            {
+                return true;
+            }
+
+            else
+            {
+                IEnumerable<PawnKindDef> source = DefDatabase<PawnKindDef>.AllDefs.Where((PawnKindDef k) => k.RaceProps.Animal && k.canArriveManhunter && (tile == -1 || Find.World.tileTemperatures.SeasonAndOutdoorTemperatureAcceptableFor(tile, k.race)));
+                source = source.Where(k => k.modContentPack?.Name == "Biomes! Prehistoric");
+                if (source.Any())
+                {
+                    if (source.TryRandomElementByWeight((PawnKindDef a) => ManhunterPackIncidentUtility.ManhunterAnimalWeight(a, points), out animalKind))
+                    {
+                        __result = true;
+                        return false;
+
+                    }
+                    else if (points > source.Min((PawnKindDef a) => a.combatPower) * 2f)
+                    {
+                        animalKind = source.MaxBy((PawnKindDef a) => a.combatPower);
+                        __result = true;
+                        return false;
+
+                    }
+                }
+
+                animalKind = null;
+                __result = false;
+                return false;
+            }
+            
+        }
+    }
+
+
+
+    /// <summary>
+    /// patch replaces pack animals in AI trader caravans with dinos
+    /// </summary>
+    [HarmonyPatch(typeof(PawnGroupKindWorker_Trader), "GenerateCarriers")]
+    public static class TraderPackAnimalPatch
+    {
+        static void Postfix(ref List<Pawn> outPawns)
+        {
+            if (BiomesPrehistoricMod.mod.settings.dinoOnly)
+            {
+                foreach(Pawn pawn in outPawns)
+                {
+                    Log.Message("PAWN KIND: " + pawn.kindDef.defName);
+
+                    switch (pawn.kindDef.defName)
+                    {
+                        case "Muffalo":
+                            pawn.kindDef = PawnKindDef.Named("BMT_Pachyrhinosaurus");
+                            break;
+                        case "Bison":
+                            pawn.kindDef = PawnKindDef.Named("BMT_Apatosaurus");
+                            break;
+                        case "Dromedary":
+                            pawn.kindDef = PawnKindDef.Named("BMT_Parasaur");
+                            break;
+                        case "Yak":
+                            pawn.kindDef = PawnKindDef.Named("BMT_Pachyrhinosaurus");
+                            break;
+                        case "Horse":
+                            pawn.kindDef = PawnKindDef.Named("BMT_Gallimimus");
+                            break;
+                        case "Donkey":
+                            pawn.kindDef = PawnKindDef.Named("BMT_Gallimimus");
+                            break;
+                        case "Elephant":
+                            pawn.kindDef = PawnKindDef.Named("BMT_Brachiosaurus");
+                            break;
+                        case "Alpaca":
+                            pawn.kindDef = PawnKindDef.Named("BMT_Iguanodon");
+                            break;
+                        //default:
+                        //    pawn.kindDef = PawnKindDef.Named("BMT_Brachiosaurus");
+                        //    break;
+
+                    }
+
+                }
+            }
+
+        }
+    }
+
+
+
+
 
     // Uncomment this patch for Dino World to apply to plants
 
@@ -42,9 +142,9 @@ namespace BiomesPrehistoric
         {
             if (BiomesPrehistoricMod.mod.settings.dinoOnly)
             {
-                if (___wildPlants.Any(d => d.animal.modContentPack.Name == "Biomes! Prehistoric"))
+                if (___wildPlants?.Any(d => d.animal.modContentPack?.Name == "Biomes! Prehistoric") == true)
                 {
-                    if (plantDef.modContentPack.Name != "Biomes! Prehistoric")
+                    if (plantDef.modContentPack?.Name != "Biomes! Prehistoric")
                     {
                         __result = 0f;
                         return false;
@@ -56,4 +156,7 @@ namespace BiomesPrehistoric
         }
     }
     */
+
+
+
 }
