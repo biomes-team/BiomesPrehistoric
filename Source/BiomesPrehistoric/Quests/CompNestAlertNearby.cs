592be464bc0a489fac882b223ba498f421c38cb4
m00nl1ght <github@m00nl1ght.dev>
2022-08-24 23:15:17 +0200

    Implement CompNestAlertNearby for nesting site quest

---
+++ Source/BiomesPrehistoric/Quests/CompNestAlertNearby.cs
@@ -0,0 +1,102 @@
+using System;
+using System.Diagnostics.CodeAnalysis;
+using System.Linq;
+using RimWorld;
+using Verse;
+using Verse.AI;
+
+namespace BiomesPrehistoric
+{
+    public class CompNestAlertNearby : ThingComp
+    {
+        private bool _triggered;
+        private bool _enabled;
+
+        public CompProperties_NestAlertNearby Props => (CompProperties_NestAlertNearby) props;
+
+        public void Enable() => _enabled = true;
+
+        public override void CompTick()
+        {
+            base.CompTick();
+            
+            if (!_enabled || _triggered || !parent.Spawned)
+                return;
+            
+            if (Find.TickManager.TicksGame % 250 == 0)
+                CompTickRare();
+        }
+
+        public override void CompTickRare()
+        {
+            base.CompTickRare();
+
+            if (!_enabled || _triggered || !parent.Spawned)
+                return;
+
+            var validator = (Predicate<Thing>) null;
+            if (Props.onlyHumanlike)
+                validator = t => t is Pawn pawn && pawn.RaceProps.Humanlike;
+            
+            var thing = (Thing) null;
+            
+            if (Props.triggerRadius > 0.0)
+                thing = GenClosest.ClosestThingReachable(parent.Position, parent.Map, 
+                    ThingRequest.ForGroup(ThingRequestGroup.Pawn), PathEndMode.OnCell, 
+                    TraverseParms.For(TraverseMode.NoPassClosedDoors), Props.triggerRadius, validator);
+            
+            Log.Message("tick: " + thing?.def.defName);
+            
+            if (thing == null)
+                return;
+            
+            var eggContainer = parent.TryGetComp<CompEggContainer>();
+            if (eggContainer == null || !eggContainer.innerContainer.Any) return;
+
+            var eggStack = eggContainer.innerContainer[0];
+            
+            var hatcher = eggStack.TryGetComp<CompHatcher>();
+            if (hatcher == null) return;
+
+            var dinoKind = hatcher.Props.hatcherPawn;
+            var dinos = parent.Map.mapPawns.AllPawnsSpawned
+                .Where(p => p.kindDef == dinoKind && p.Position.InHorDistOf(parent.Position, Props.alertRadius));
+            
+            Log.Message("dinoKind: " + dinoKind + " found " + dinos.Count());
+
+            bool foundOne = false;
+            foreach (var dino in dinos)
+            {
+                if (dino.mindState.mentalStateHandler.TryStartMentalState(MentalStateDefOf.Manhunter))
+                {
+                    LifeStageUtility.PlayNearestLifestageSound(dino, ls => ls.soundAngry);
+                    foundOne = true;
+                }
+            }
+
+            if (foundOne)
+            {
+                Messages.Message("BMT_NestAlert".Translate((NamedArgument) thing), (Thing) parent, MessageTypeDefOf.ThreatBig);
+                Find.CameraDriver.shaker.DoShake(2f);
+            }
+
+            _triggered = true;
+        }
+
+        public override void PostExposeData()
+        {
+            Scribe_Values.Look(ref _triggered, "triggered");
+            Scribe_Values.Look(ref _enabled, "enabled");
+        }
+    }
+
+    [SuppressMessage("ReSharper", "InconsistentNaming")]
+    public class CompProperties_NestAlertNearby : CompProperties
+    {
+        public bool onlyHumanlike;
+        public float triggerRadius;
+        public float alertRadius;
+
+        public CompProperties_NestAlertNearby() => compClass = typeof(CompNestAlertNearby);
+    }
+}
--- Source/BiomesPrehistoric/Quests/SitePartWorker_NestingSite.cs
+++ Source/BiomesPrehistoric/Quests/SitePartWorker_NestingSite.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using RimWorld;
@@ -19,46 +20,57 @@ namespace BiomesPrehistoric
             base.Notify_GeneratedByQuestGen(part, slate, outExtraDescriptionRules, outExtraDescriptionConstants);
 
             var tile = slate.Get<int>("siteTile");
+            var points = slate.Get<int>("points");
 
             part.things = new ThingOwner<Thing>(part, false);
 
             var biome = tile < 0 ? BiomeDefOf.TemperateForest : Find.World?.grid[tile]?.biome ?? BiomeDefOf.TemperateForest;
 
             var dinoDefs = biome.AllWildAnimals
-                .Select(pawnKind => pawnKind.race)
                 .Where(animalDef => animalDef != null && animalDef.modContentPack == BiomesPrehistoricMod.ModContentPack)
-                .Where(animalDef => animalDef.comps.Any(comp => comp is CompProperties_EggLayer))
+                .Where(animalDef => animalDef.race.comps.Any(comp => comp is CompProperties_EggLayer))
                 .ToList();
 
             if (dinoDefs.Count == 0)
             {
                 Log.Warning("Failed to get dino type for biome, using any type instead.");
                 dinoDefs = DefDatabase<PawnKindDef>.AllDefs
-                    .Select(pawnKind => pawnKind.race)
                     .Where(animalDef => animalDef != null && animalDef.modContentPack == BiomesPrehistoricMod.ModContentPack)
-                    .Where(animalDef => animalDef.comps.Any(comp => comp is CompProperties_EggLayer))
+                    .Where(animalDef => animalDef.race.comps.Any(comp => comp is CompProperties_EggLayer))
                     .ToList();
             }
             
             var dinoDef = dinoDefs.RandomElement();
-            var isLargeDino = BiomesPrehistoric.IsLargeDino(dinoDef);
-            var nestCount = Rand.Range(2, 10);
+            var isLargeDino = BiomesPrehistoric.IsLargeDino(dinoDef.race);
+            var nestCount = Math.Min(10, (int)(points / 1000f) + Rand.Range(1, 3));
+            var adultCount = Math.Min(20, (int)(points / 1000f) * 2 + Rand.Range(2, 5));
             var nestDef = isLargeDino ? BiomesPrehistoric_DefOf.BMT_LargeNest : BiomesPrehistoric_DefOf.BMT_SmallNest;
 
-            var foundComp = dinoDef.comps.FirstOrDefault(comp => comp is CompProperties_EggLayer);
+            var foundComp = dinoDef.race.comps.FirstOrDefault(comp => comp is CompProperties_EggLayer);
             if (foundComp is CompProperties_EggLayer eggLayer)
             {
                 for (int i = 0; i < nestCount; i++)
                 {
                     var nest = ThingMaker.MakeThing(nestDef);
+                    if (nest == null) continue;
+
+                    var nestAlertNearby = nest.TryGetComp<CompNestAlertNearby>();
+                    nestAlertNearby?.Enable();
+
                     var eggContainer = nest.TryGetComp<CompEggContainer>();
-                    if (eggContainer != null)
-                    {
-                        var eggs = ThingMaker.MakeThing(eggLayer.eggFertilizedDef);
-                        eggs.stackCount = eggLayer.eggCountRange.RandomInRange;
-                        eggContainer.innerContainer.TryAdd(eggs);
-                        part.things.TryAdd(nest, false);
-                    }
+                    if (eggContainer == null) continue;
+                    
+                    var eggs = ThingMaker.MakeThing(eggLayer.eggFertilizedDef);
+                    eggs.stackCount = eggLayer.eggCountRange.RandomInRange;
+                    eggContainer.innerContainer.TryAdd(eggs);
+                    
+                    part.things.TryAdd(nest, false);
+                }
+                
+                for (int i = 0; i < adultCount; i++)
+                {
+                    var dino = PawnGenerator.GeneratePawn(dinoDef);
+                    part.things.TryAdd(dino, false);
                 }
             }
         }
