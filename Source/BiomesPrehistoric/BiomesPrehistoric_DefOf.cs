5624c892925639809ad168e606360b4f9f518306
XidonEye <77588639+XidonEye@users.noreply.github.com>
2022-11-15 22:32:41 +0100

    initial

--- Source/BiomesPrehistoric/BiomesPrehistoric_DefOf.cs
+++ Source/BiomesPrehistoric/BiomesPrehistoric_DefOf.cs
@@ -10,5 +10,8 @@ namespace BiomesPrehistoric
     {
         public static ThingDef BMT_SmallNest;
         public static ThingDef BMT_LargeNest;
+        public static ThingDef Filth_GelatinousSlime;
+        public static ThingDef BMT_GelatinousRaptorEggIncoming;
+        public static ThingDef BMT_GelatinousRaptorEgg;
     }
 }
