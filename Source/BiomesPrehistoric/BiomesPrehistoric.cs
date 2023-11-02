ad4073d6edf4b3fc820ef30d013d65c6bdd237fd
José Ángel Soler Ortiz <jose.angel.soler.ortiz@gmail.com>
2023-11-02 22:36:14 +0100

    Bump version to 1.1.0.

--- Source/BiomesPrehistoric/BiomesPrehistoric.cs
+++ Source/BiomesPrehistoric/BiomesPrehistoric.cs
@@ -1,24 +1,32 @@
-﻿using HarmonyLib;
+﻿using System;
+using HarmonyLib;
 using Verse;
 
 namespace BiomesPrehistoric
 {
-    [StaticConstructorOnStartup]
-    static class BiomesPrehistoric
-    {
-        static BiomesPrehistoric()
-        {
-            Harmony harmony = new Harmony("rimworld.biomesprehistoric");
+	[StaticConstructorOnStartup]
+	static class BiomesPrehistoric
+	{
+		public const string Name = "Biomes! Prehistoric";
+		private static readonly Version Version = typeof(BiomesPrehistoric).Assembly.GetName().Version;
+		static BiomesPrehistoric()
+		{
+			Harmony harmony = new Harmony("rimworld.biomesprehistoric");
 
-            harmony.PatchAll();
-            Log.Message("Biomes! Prehistoric initialized");
-            
-        //    BiomesPrehistoricMod.UpdateMainMenuSongDef();
-        }
+			harmony.PatchAll();
+			Log("Initialized");
 
-        public static bool IsLargeDino(ThingDef def)
-        {
-            return def.race?.baseBodySize >= 3f;
-        }
-    }
+			//    BiomesPrehistoricMod.UpdateMainMenuSongDef();
+		}
+
+		public static void Log(string message) => Verse.Log.Message(PrefixMessage(message));
+		public static void Error(string message) => Verse.Log.Error(PrefixMessage(message));
+
+		private static string PrefixMessage(string message) => $"[{Name} v{Version}] {message}";
+
+		public static bool IsLargeDino(ThingDef def)
+		{
+			return def.race?.baseBodySize >= 3f;
+		}
+	}
 }
